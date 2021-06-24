b081cfcaf83580fecc1194f9cf3a0be7dab50de4
ota-yuto <79303376+ota-yuto@users.noreply.github.com>
2021-06-24 19:04:09 +0900

    test
    committest

---
+++ JavaStudy/2-5/src/study/Study.java
@@ -0,0 +1,49 @@
+package study;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 本課題では、コレクションフレームワークのList系クラスとMap系クラスの基本的な記述を学びましょう。
+ * 現場で必ず使用するものなので、ポイントをしっかり押さえておきましょう。
+ *
+ * 問①〜問③まであります。
+ * 問②に関しては、コメントを記述してください。
+ *
+ */
+public class Study {
+
+    public static final String SHOP_SHOHIN_00 = "バナナ";
+    public static final String SHOP_SHOHIN_01 = "牛乳";
+    public static final String SHOP_SHOHIN_02 = "豚肉";
+    public static final String SHOP_SHOHIN_03 = "コロッケ";
+
+    public static void main(String args[]) {
+
+        // ① 定数を全て使って、String型のListを記述してください。
+Map<String,Integer>linkedHashMap = new LinkedHashMap<String,Integer>();
+
+
+
+
+        // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
+        /*
+         *
+         *
+         */
+        LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
+        shopMap.put(shohinList.get(0), 125);
+        shopMap.put(shohinList.get(1), 180);
+        shopMap.put(shohinList.get(2), 350);
+        shopMap.put(shohinList.get(3), 100);
+
+        // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
+        // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
+for (String SHOP_SHOHIN_00: linkedHashMap.put()) {
+	System.out.println("SHOP_SHOHIN_00="+"shohinlist"+"になります！");
+}
+
+
+
+    }
+}
